9751964b06f5f4c873eddf3db1e263cbbfb09171
Kody Dudley <kodymdudley@gmail.com>
2020-10-26 12:46:59 -0600

    edits

--- app/Services/TodoService.js
+++ app/Services/TodoService.js
@@ -27,18 +27,18 @@ class TodoService {
 
   async toggleTodoStatus(todoId) {
     let todo = await ProxyState.todos.find(todo => todo.id == todoId);
+    todo.completed = todo.completed ? false : true
     //TODO Make sure that you found a todo,
     //		and if you did find one
     //		change its completed status to whatever it is not (ex: false => true or true => false)
 
     let res = await api.put(url + todoId, todo);
-    //TODO how do you trigger this change
+    this.getTodos()
   }
 
   async removeTodo(todoId) {
-    //TODO Work through this one on your own
-    //		what is the request type
-    //		once the response comes back, how do you update the state
+    let res = await api.delete(url + todoId)
+    this.getTodos()
   }
 }
 
--- app/models/Todo.js
+++ app/models/Todo.js
@@ -1,21 +1,40 @@
 export default class Todo {
-  constructor({
-    data
-  }) {
+  constructor(data) {
     this.description = data.description,
       this.id = data._id;
+    this.completed = data.completed
   }
 
   get Template() {
-    return /*html*/ `
-    <form onsubmit="app.todoController.addTodo(e)">
-  <div class="form-group">
-    <label for="todos">Todos</label>
-    <input type="text" class="form-control" id="description" aria-describedby="emailHelp" placeholder="Enter Todo">
-  </div>
-  <button onclick="submit" type="submit" class="btn btn-primary">Submit</button>
-</form>
+    if (this.completed) {
+      return /*html*/ `
+  <div class="row">
+    <div class="col-2">
+      <button onclick="app.todoController.toggleTodoStatus('${this.id}')" class="btn btn-success"><i class="fas fa-check-square"></i></button>
+    </div>
+    <div class="col-8 bg-dark text-light">
+      <h6 class="line-through text-center ">${this.description}</h6>
+    </div>
+    <div class="col-2">
+      <button onclick="app.todoController.removeTodo('${this.id}')" class="btn btn-danger"><i class="fas fa-minus-circle"></i></button>
+    </div>
+</div>
     `
+    } else {
+      return /*html*/ `
+  <div class="row">
+    <div class="col-2">
+      <button onclick="app.todoController.toggleTodoStatus('${this.id}')" class="btn btn-success"><i class="fas fa-check-square"></i></button>
+    </div>
+    <div class="col-8 bg-dark text-light">
+      <h6 class="text-center">${this.description}</h6>
+    </div>
+    <div class="col-2">
+      <button onclick="app.todoController.removeTodo('${this.id}')" class="btn btn-danger"><i class="fas fa-minus-circle"></i></button>
+    </div>
+</div>
+    `
+    }
   }
 
 }
